f106f4b7b42397b8366989914f82b54c512cd7a3
Tom Barnsbury <ruralmyth@gmail.com>
2019-01-29 08:45:31 +0000

    Don't use relative paths in download-wav script

--- bin/download-wav.sh
+++ bin/download-wav.sh
@@ -8,7 +8,9 @@ URL=$1
 FILE_PATH=$2
 TITLE=$3
 
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo "downloading ${TITLE}"
 curl -s ${URL} --output ${FILE_PATH}
 
-./bin/transfer-file-to-media-centre.sh ${TITLE} ${FILE_PATH}
+source ${__dir}/transfer-file-to-media-centre.sh ${TITLE} ${FILE_PATH}
