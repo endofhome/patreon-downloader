182e62483c0730c5906155ef24ea576146a8fecb
Tom Barnsbury <ruralmyth@gmail.com>
2019-02-03 22:06:09 +0000

    Quote file paths in case of spaces

--- bin/download-mp3.sh
+++ bin/download-mp3.sh
@@ -14,20 +14,20 @@ ARTWORK_URL=$6
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "downloading ${TITLE}"
-curl -s ${URL} --output ${FILE_PATH}
+curl -s ${URL} --output "${FILE_PATH}"
 
 echo "tagging ${TITLE}"
-id3 -c "$COMMENT" -y ${YEAR} ${FILE_PATH} >/dev/null 2>&1
+id3 -c "$COMMENT" -y ${YEAR} "${FILE_PATH}" >/dev/null 2>&1
 
 echo "downloading artwork for ${TITLE}"
 TITLE_NO_WHITESPACE=$(echo ${TITLE} | sed -e 's/ /_/g')
 ARTWORK_FILE_PATH="$(dirname "${BASH_SOURCE[0]}")/../files/cover-${TITLE_NO_WHITESPACE}.jpg"
-curl -s ${ARTWORK_URL} --output ${ARTWORK_FILE_PATH}
+curl -s ${ARTWORK_URL} --output "${ARTWORK_FILE_PATH}"
 
 echo "embedding artwork"
-eyeD3 --add-image "${ARTWORK_FILE_PATH}:FRONT_COVER" ${FILE_PATH} >/dev/null 2>&1
+eyeD3 --add-image "${ARTWORK_FILE_PATH}:FRONT_COVER" "${FILE_PATH}" >/dev/null 2>&1
 
-rm ${ARTWORK_FILE_PATH}
+rm "${ARTWORK_FILE_PATH}"
 
-source ${__dir}/transfer-file-to-media-centre.sh "${TITLE}" ${FILE_PATH}
+source ${__dir}/transfer-file-to-media-centre.sh "${TITLE}" "${FILE_PATH}"
 
