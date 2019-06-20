be73a1e5fa8e7598068f9eb663175dbed84a1a8a
Tom Barnsbury <ruralmyth@gmail.com>
2019-06-20 17:00:25 +0100

    Don't print output of library update command

--- bin/update-kodi-audio-library.sh
+++ bin/update-kodi-audio-library.sh
@@ -5,4 +5,4 @@ set -o errexit
 set -o pipefail
 
 echo "updating Kodi audio library"
-curl -s --data-binary '{ "jsonrpc": "2.0", "method": "AudioLibrary.Scan", "id": "patreon-downloader"}' -H 'content-type: application/json;' http://${KODI_USERNAME}:${KODI_PASSWORD}@${DESTINATION_MACHINE_HOST}:${KODI_PORT}/jsonrpc
+curl -s --data-binary '{ "jsonrpc": "2.0", "method": "AudioLibrary.Scan", "id": "patreon-downloader"}' -H 'content-type: application/json;' http://${KODI_USERNAME}:${KODI_PASSWORD}@${DESTINATION_MACHINE_HOST}:${KODI_PORT}/jsonrpc >/dev/null 2>&1
