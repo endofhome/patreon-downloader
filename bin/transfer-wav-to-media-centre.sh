c9a1d25a8ea6ad692f8caca42f95b7641d3f0f19
Tom Barnsbury <ruralmyth@gmail.com>
2019-06-22 11:09:21 +0100

    Fix typos and add additional logging when transferring files to media centre

--- bin/transfer-wav-to-media-centre.sh
+++ bin/transfer-wav-to-media-centre.sh
@@ -20,16 +20,18 @@ else
 fi
 
 if [[ $(uname -a | awk '{print $2}') = ${DESTINATION_MACHINE_NAME} ]]; then
+  echo "destination directory located on this machine, using mv"
   mv "${FILE_PATH}" "${DESTINATION}"
   mv "${CUE_FILE_PATH}" "${DESTINATION}"
   mv "${ARTWORK_FILE_PATH}" "${DESTINATION}"
 else
+  echo "destination directory located on a remote machine, using scp"
   scp "${FILE_PATH}" ${DESTINATION_MACHINE_USERNAME}@${DESTINATION_MACHINE_HOST}:"${DESTINATION}"
   scp "${CUE_FILE_PATH}" ${DESTINATION_MACHINE_USERNAME}@${DESTINATION_MACHINE_HOST}:"${DESTINATION}"
   scp "${ARTWORK_FILE_PATH}" ${DESTINATION_MACHINE_USERNAME}@${DESTINATION_MACHINE_HOST}:"${DESTINATION}"
-  rm "{$FILE_PATH}"
-  rm "{$CUE_FILE_PATH}"
-  rm "{$ARTWORK_FILE_PATH}"
+  rm "${FILE_PATH}"
+  rm "${CUE_FILE_PATH}"
+  rm "${ARTWORK_FILE_PATH}"
 fi
 
 "${__dir}/update-kodi-audio-library.sh"
