ee491a57952fa07afc720ed5ae665b04d48121e3
Tom Barnsbury <ruralmyth@gmail.com>
2019-06-24 22:45:56 +0100

    Scroll down half the viewport at a time and click 'Load more' if present - site does not infinite scroll any more.

--- js/lazyLoadImages.js
+++ js/lazyLoadImages.js
@@ -11,9 +11,24 @@ module.exports = async function getLazyLoadedImages(page, waitTime) {
 
     const viewportHeight = page.viewport().height;
     let viewportIncr = 0;
-    while (viewportIncr + viewportHeight < height) {
+    // (height * 8) is a bit sloppy - I need to spend some time to find a better solution.
+    while (viewportIncr + viewportHeight < height * 8) {
         await page.evaluate(_viewportHeight => {
-            window.scrollBy(0, _viewportHeight);
+            // scroll by half of the viewport at a time
+            window.scrollBy(0, _viewportHeight / 2);
+
+            const buttons = [...document.querySelectorAll('button')];
+            const loadMoreButton = buttons.find(button => {
+                const loadMoreDiv = [...button.querySelectorAll('div')].find(div => {
+                    return div.textContent === 'Load more'
+                });
+                if (loadMoreDiv !== undefined) {
+                    return button;
+                }
+            });
+            if (loadMoreButton !== undefined) {
+                loadMoreButton.click();
+            }
         }, viewportHeight);
         await wait(waitTime * 1000);
         viewportIncr = viewportIncr + viewportHeight;
